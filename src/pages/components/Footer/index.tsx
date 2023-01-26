d633ee5c6da9a4966cbcba15e4d4ba4558d9eaff
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-01-26 19:34:41 -0300

    chore: starting footer

---
+++ src/pages/components/Footer/index.tsx
@@ -0,0 +1,62 @@
+export default function Footer() {
+  const footer_content = [
+    {
+      subject: {
+        title: "Learn",
+        subtitles: [
+          "Introduction",
+          "Features",
+          "Staking",
+          "Get HEDRON",
+          "FAQ",
+        ],
+      },
+    },
+    {
+      build: {
+        title: "Build",
+        subtitles: [
+          "Developer Portal",
+          "Ignite CLI",
+          "Cronos SDK",
+          "IBC Protocol",
+        ],
+      },
+    },
+    {
+      explore: {
+        title: "Explore",
+        subtitles: [
+          "Tokens",
+          "Apps & Service",
+          "Wallets",
+          "Gravity DEX",
+          "Blog",
+        ],
+      },
+    },
+    {
+      participate: {
+        title: "Participate",
+        subtitles: [
+          "Community",
+          "Contributors",
+          "Events",
+          "Newsletters",
+        ],
+      },
+    },
+    {
+      resources: {
+        title: "Resources",
+        subtitles: [
+          "About",
+          "Press Kit",
+          "Design",
+          "Resources",
+        ],
+      },
+    },
+  ];
+  return <div>Hello, World</div>;
+}
