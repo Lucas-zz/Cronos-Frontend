5999e9df97ae114f8e3e08ee6410be545e9121d5
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-01-26 21:01:25 -0300

    feat: responsive footer finished with some small animations on hover

--- src/pages/components/Footer/index.tsx
+++ src/pages/components/Footer/index.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion"
+
 export default function Footer() {
   const footer_content = [
     {
@@ -13,7 +15,7 @@ export default function Footer() {
       },
     },
     {
-      build: {
+      subject: {
         title: "Build",
         subtitles: [
           "Developer Portal",
@@ -24,7 +26,7 @@ export default function Footer() {
       },
     },
     {
-      explore: {
+      subject: {
         title: "Explore",
         subtitles: [
           "Tokens",
@@ -36,7 +38,7 @@ export default function Footer() {
       },
     },
     {
-      participate: {
+      subject: {
         title: "Participate",
         subtitles: [
           "Community",
@@ -47,7 +49,7 @@ export default function Footer() {
       },
     },
     {
-      resources: {
+      subject: {
         title: "Resources",
         subtitles: [
           "About",
@@ -58,5 +60,21 @@ export default function Footer() {
       },
     },
   ];
-  return <div>Hello, World</div>;
+  return (
+    <footer className="max-w-screen-xl mx-auto shadow-md bg-bgBlack relative z-10 select-none py-36">
+      <motion.nav layout transition={{ duration: 0.3 }} className="grid grid-cols-2 justify:center gap-10 m-auto sm:grid-cols-3 lg:w-full w-fit lg:flex lg:gap-5 lg:justify-between py-6 sm:py-8 md:py:16 lg:py-16 px-8 md:px-16 lg:px-14 xl:px-10 2xl:px-0 font-md">
+        {
+          footer_content &&
+          footer_content.map(({ subject }) => (
+            <ul className="justify-center" key={subject.title}>
+              <li className="text-xl md:text-2xl pb-10 font-medium" key={subject.title}>{subject.title}</li>
+              {subject.subtitles.map((subtitle, i) => (
+                <motion.li whileHover={{ translateX: 10 }} transition={{ type: "spring", stiffness: 200 }} className="text-base md:text-lg opacity-50 font-normal pb-4 hover:underline cursor-pointer" key={i}>{subtitle}</motion.li>
+              ))}
+            </ul>
+          ))
+        }
+      </motion.nav>
+    </footer>
+  );
 }
