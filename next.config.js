34634bb5ff118d0a91eb2030bd5cae58c2bdff2f
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-01-29 04:56:20 -0300

    fix: deployment fixes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js', 'index.tsx', 'index.jsx']
 }
 
 module.exports = nextConfig
